6735ff42fae861f7beeb36ae5df84d097f73fba1
GavrilovaAnastasia <m1907905@edu.misis.ru>
2021-01-08 18:18:39 +0300

    Initial commit

---
+++ src/Sqlite3QT_ru_RU.ts
@@ -0,0 +1,289 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="Sqlite3QT_ru_RU">
+<context>
+    <name>AdminWindow</name>
+    <message>
+        <location filename="adminwindow.ui" line="14"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="198"/>
+        <source>Администрирование БД</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="31"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="208"/>
+        <source>Таблицы</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="82"/>
+        <location filename="adminwindow.ui" line="105"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="210"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="212"/>
+        <source>Добавить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="89"/>
+        <location filename="adminwindow.ui" line="112"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="211"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="213"/>
+        <source>Удалить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="47"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="209"/>
+        <source>Атрибуты</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="126"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="214"/>
+        <source>Переименовать</source>
+        <oldsource>Изменить</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="133"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="215"/>
+        <source>Сделать FK</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="158"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="216"/>
+        <source>Файл</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="169"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="199"/>
+        <source>Выйти из учетной записи</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="174"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="200"/>
+        <source>Выйти</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="177"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="202"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="182"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="204"/>
+        <source>Обновить</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="adminwindow.ui" line="185"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_adminwindow.h" line="206"/>
+        <source>F5</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ColumnCreator</name>
+    <message>
+        <location filename="columncreator.ui" line="14"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_columncreator.h" line="73"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="columncreator.ui" line="32"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_columncreator.h" line="74"/>
+        <source>Введите название колонки</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="columncreator.ui" line="45"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_columncreator.h" line="75"/>
+        <source>Ввод</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ColumnRenamer</name>
+    <message>
+        <location filename="columnrenamer.ui" line="14"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_columnrenamer.h" line="67"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="columnrenamer.ui" line="30"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_columnrenamer.h" line="68"/>
+        <source>Введите новое название колонки</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="columnrenamer.ui" line="43"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_columnrenamer.h" line="69"/>
+        <source>Ввод</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>EntityCreator</name>
+    <message>
+        <location filename="entitycreator.ui" line="23"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_entitycreator.h" line="87"/>
+        <source>Создание</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entitycreator.ui" line="49"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_entitycreator.h" line="88"/>
+        <source>Введите название таблицы</source>
+        <oldsource>Введите название</oldsource>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="entitycreator.ui" line="65"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_entitycreator.h" line="89"/>
+        <source>Ввод</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>FKCreator</name>
+    <message>
+        <location filename="fkcreator.ui" line="14"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_fkcreator.h" line="74"/>
+        <source>Dialog</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fkcreator.ui" line="30"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_fkcreator.h" line="75"/>
+        <source>Выберите таблицу</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="fkcreator.ui" line="48"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_fkcreator.h" line="76"/>
+        <source>Выбрать</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>LoginWindow</name>
+    <message>
+        <location filename="loginwindow.ui" line="14"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_loginwindow.h" line="198"/>
+        <source>Авторизация</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwindow.ui" line="154"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_loginwindow.h" line="204"/>
+        <source>Администратор</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwindow.ui" line="161"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_loginwindow.h" line="205"/>
+        <source>Оператор</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwindow.ui" line="25"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_loginwindow.h" line="199"/>
+        <source>Создать</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwindow.ui" line="45"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_loginwindow.h" line="200"/>
+        <source>...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwindow.ui" line="54"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_loginwindow.h" line="201"/>
+        <source>Открыть</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwindow.ui" line="116"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_loginwindow.h" line="202"/>
+        <source>Логин:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwindow.ui" line="138"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_loginwindow.h" line="203"/>
+        <source>Пароль:</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="loginwindow.ui" line="185"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_loginwindow.h" line="206"/>
+        <source>Войти</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="20"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_mainwindow.h" line="165"/>
+        <source>Оперирование БД</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="54"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_mainwindow.h" line="171"/>
+        <source>Изменить значение</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="106"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_mainwindow.h" line="173"/>
+        <source>Добавить строку</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="113"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_mainwindow.h" line="174"/>
+        <source>Удалить строку</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="79"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_mainwindow.h" line="172"/>
+        <source>Выбрать таблицу</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="134"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_mainwindow.h" line="175"/>
+        <source>Файл</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="144"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_mainwindow.h" line="166"/>
+        <source>Выйти из учетной записи</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="149"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_mainwindow.h" line="167"/>
+        <source>Выйти</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="152"/>
+        <location filename="../build/src/Sqlite3QT_autogen/include_Release/ui_mainwindow.h" line="169"/>
+        <source>Ctrl+Q</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
